c60d5b5eb39dfccbc7dfa885c976d11bba8f4497
yanakachur-se <yana.kachur@gmail.com>
2020-03-17 10:57:16 +0100

    Input feature added

--- src/edu/sda/Bar.java
+++ src/edu/sda/Bar.java
@@ -1,4 +1,37 @@
 package edu.sda;
 
 public class Bar {
+    String name;
+    double fett;
+    double protein;
+    double fiber;
+    String reviewer;
+
+    public Bar(String name, double fett, double protein, double fiber, String reviewer) {
+        this.name = name;
+        this.fett = fett;
+        this.protein = protein;
+        this.fiber = fiber;
+        this.reviewer = reviewer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getFett() {
+        return fett;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getFiber() {
+        return fiber;
+    }
+
+    public String getReviewer() {
+        return reviewer;
+    }
 }
--- src/edu/sda/BarService.java
+++ src/edu/sda/BarService.java
@@ -1,4 +1,40 @@
 package edu.sda;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class BarService {
+
+    List<Bar> bars;
+
+    public void initBars() {
+        Parser parser = new Parser();
+        bars = parser.fetchBars();
+    }
+
+    void fetchNames() {
+        bars.stream().map(Bar::getName).forEach(System.out::println);
+    }
+
+    void sortByHighProtein() {
+        bars.stream()
+                .sorted(Comparator.comparingDouble(Bar::getProtein))
+                .map(Bar::getName)
+                .forEach(System.out::println);
+    }
+
+    void sortByHighFett() {
+        bars.stream()
+                .sorted(Comparator.comparingDouble(Bar::getFett))
+                .map(Bar::getName)
+                .forEach(System.out::println);
+    }
+
+    void filterByLowFiberSort(double userFiber) {
+        bars.stream()
+                .filter(bar -> bar.getFiber() < userFiber)
+                .sorted(Comparator.comparingDouble(Bar::getFiber))
+                .map(Bar::getName)
+                .forEach(System.out::println);
+    }
 }
